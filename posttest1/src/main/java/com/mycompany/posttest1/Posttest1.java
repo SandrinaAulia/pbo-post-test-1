627e42cf1ea1acc6873c524a4d6417868fef0b52
SandrinaAulia <121924124+SandrinaAulia@users.noreply.github.com>
2023-09-27 19:31:54 +0800

    Posttest1.java

--- posttest1/src/main/java/com/mycompany/posttest1/Posttest1.java
+++ posttest1/src/main/java/com/mycompany/posttest1/Posttest1.java
@@ -12,7 +12,8 @@ import java.util.ArrayList;
 public class Posttest1 {
 
     public static void main(String[] args) {
-        ArrayList<Karyawan> karyawans = new ArrayList<>();
+        ArrayList<Karyawan> karyawans = new ArrayList<>(); //membuat ArrayList untuk
+        //menyimpan objek dari karyawan
         
         //inisialisasi 5 objek
         Karyawan karyawansatu = new Karyawan("Sandrina", "202001021001", "CEO", 4.5f, 25000000);
@@ -33,16 +34,20 @@ public class Posttest1 {
         for (Karyawan karyawan : karyawans) {
             result.append(karyawan.nama_karyawan);
             result.append("(" + karyawan.nomor_karyawan + ")")
-              .append("adalah karyawan dengan jabatan sebagai")
+              .append("adalah karyawan dengan jabatan sebagai ")
               .append(karyawan.jabatan)
-              .append("dengan rating kinerja yakni")
+              .append(" dengan rating kinerja yakni ")
               .append(karyawan.rating_kinerja)
+              .append(" dan gaji sebesar ")
+              .append(karyawan.gaji)
               .append(".\n");
               
         }
         
         //menampilkan hasil penggabungan
+        System.out.println("=====================================");
         System.out.println(">>>>> Data Karyawan Terkini <<<<<");
+        System.out.println("=====================================");
         System.out.println(result.toString());
         
         }
